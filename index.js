dc8dcd2ecc4dd90e288d63f1cfab8d3f7412529e
Mokgome Sechaba Ntshehi <u19062053@tuks.co.za>
2025-01-28 16:58:04 +0200

    Finished off socketio example project. To-Do: my own improvements

--- index.js
+++ index.js
@@ -9,64 +9,97 @@ import { Server } from 'socket.io'
 import sqlite3 from 'sqlite3'
 import { open } from 'sqlite'
 
-const db = await open({
-    filename: 'chat.db',
-    driver: sqlite3.Database
-})
+import { availableParallelism } from 'node:os'
+import cluster from 'node:cluster'
+import { createAdapter, setupPrimary } from '@socket.io/cluster-adapter'
 
-await db.exec(`
-    CREATE TABLE IF NOT EXISTS messages(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        client_offset TEXT UNIQUE,
-        content TEXT
-    );
-`)
+if(cluster.isPrimary){
+    const numCPUs = availableParallelism();
 
-const app = express()
-const server = createServer(app)
-const io = new Server(server, {
-    connectionStateRecovery: {}
-})
-const PORT = 5555;
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
-app.get('/', (req, res) =>{
-    res.sendFile(join(__dirname, "index.html"))
-})
-
-io.on('connection', async (socket) =>{
-    console.log("a user is connected yipee")
-    socket.on("chat message", async (msg)=>{
-        let result;
-
-        try{
-            console.log("result pending")
-            result = await db.run("INSERT INTO messages (content) VALUES (?)", msg) 
-            console.log("secured")
-        }
-        catch (e){
-            return
-        }
-
-        io.emit("chat message", `Message: ${msg}`, result.lastID)
-    })
-
-    if(!socket.recovered){
-        try{
-            await db.each('SELECT id, content FROM messages WHERE id > ?', [socket.handshake.auth.serverOffset || 0],
-                (_err, row)=>{
-                    socket.emit('chat message', row.content, row.id)
-                }
-            )
-        }
-        catch (e) {
-            console.log("Something went wrong")
-        }
+    for(let i = 0; i < numCPUs; i++){
+        cluster.fork({
+            PORT: 3000 + i
+        })
     }
-})
 
-server.listen(PORT, ()=>{
-    console.log(`Server on at http://localhost:${PORT}`)
-})
+    setupPrimary()
+}
+else{
+    const db = await open({
+        filename: 'chat.db',
+        driver: sqlite3.Database
+    })
+    
+    await db.exec(`
+        CREATE TABLE IF NOT EXISTS messages(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            client_offset TEXT UNIQUE,
+            content TEXT
+        );
+    `)
+    
+    const app = express()
+    const server = createServer(app)
+    const io = new Server(server, {
+        connectionStateRecovery: {},
+        adapter: createAdapter()
+    })
+    const PORT_ = process.env.PORT;
+    
+    const __dirname = dirname(fileURLToPath(import.meta.url))
+    
+    app.get('/', (req, res) =>{
+        res.sendFile(join(__dirname, "index.html"))
+    })
+    
+    io.on('connection', async (socket) =>{
+        console.log("a user is connected yipee")
+    
+        socket.on("chat message", async (msg, clientOffset, callback)=>{
+            let result;
+    
+            try{
+                console.log("result pending")
+                result = await db.run("INSERT INTO messages (content, client_offset) VALUES (?, ?)", msg, clientOffset) 
+                console.log("secured")
+    
+                io.emit("chat message", `Message: ${msg}`, result.lastID)
+                callback({
+                    status: "200 - OK"
+                })
+            }
+            catch (e){
+                if(e.errno === 19){
+                    callback({
+                        status: "Error 19 - Duplicate insert"
+                    })
+                }
+                else{
+    
+                }
+    
+                return
+            }
+        })
+    
+        if(!socket.recovered){
+            try{
+                await db.each('SELECT id, content FROM messages WHERE id > ?', [socket.handshake.auth.serverOffset || 0],
+                    (_err, row)=>{
+                        socket.emit('chat message', row.content, row.id)
+                    }
+                )
+            }
+            catch (e) {
+                console.log("Something went wrong")
+            }
+        }
+    })
+    
+    server.listen(PORT_, ()=>{
+        console.log(`Server on at http://localhost:${PORT_}`)
+    })
+}
+
+
 
